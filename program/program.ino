091fd63b97ffafa9a0d3246526d7b0a6ee92d4cf
Anung <anung093@gmail.com>
2019-09-05 21:27:37 +0700

    hapus lib timerone

--- program/program.ino
+++ program/program.ino
@@ -1,18 +1,17 @@
-//#include "TimerOne.h"
 #include <LiquidCrystal.h>
 #define jumlahLubang    48
-
 #define gatePin 4
 #define encoderPin 3
-#define potensio A0
+#define potensio A5
 #define crossIn 2
-const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+#define probe 11
+#define PULSE 70   //trigger pulse width (counts)
+const int rs = 7, en = 6, d4 = 5, d5 = 10, d6 = 9, d7 = 8;
 
 unsigned long previousMillis = 0;
 const long interval = 500; 
 int sudut;
-int PotValue ;
-int wait;
+volatile int PotValue ;
 volatile int counterEncoder;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -21,57 +20,56 @@ void interupsiEncoder(){
     counterEncoder++;
 }
 
-//void GateOn(){
-//  Timer1.stop();
-//}
-
 void interupsiPhaseCrossing(){
- // Timer1.restart();   //set our stopwatch to 0
- // delayMicroseconds(PotValue*10);
- // digitalWrite(gatePin,HIGH);
- //  delay(2);
- digitalWrite(gatePin,LOW);
- wait=PotValue*10;
- wait++;
- //  Timer1.start(); 
+  digitalWrite(gatePin,LOW);
+  digitalWrite(probe,LOW);
+  TCCR1B=0x04; //start timer with divide by 256 input
+  TCNT1 = 0;   //reset timer - count from zero 
+}
+
+ISR(TIMER1_COMPA_vect){ //comparator match
+  digitalWrite(gatePin,HIGH);  //set TRIAC gate to high
+  digitalWrite(probe,HIGH);
+  TCNT1 = 65536-PULSE;      //trigger pulse width
+}
+
+ISR(TIMER1_OVF_vect){ //timer1 overflow
+  digitalWrite(probe,LOW); //turn off TRIAC gate
+  TCCR1B = 0x00;          //disable timer stopd unintended triggers
 }
 
 void setup(){
   pinMode(gatePin, OUTPUT);
+  pinMode(probe, OUTPUT);
   pinMode(potensio, INPUT);
   pinMode(crossIn, INPUT);
   pinMode(encoderPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(crossIn), interupsiPhaseCrossing, RISING);
+  attachInterrupt(digitalPinToInterrupt(crossIn), interupsiPhaseCrossing, FALLING);
   attachInterrupt(digitalPinToInterrupt(encoderPin), interupsiEncoder, RISING);
- // Timer1.initialize(1000);  //longest pulse in is usally 20 milliseconds,
- // Timer1.attachInterrupt( GateOn ); 
- // Timer1.stop();            //stop the counter Timer1.restart();
+  TIMSK1 =0x03;    //enable comparator A and overflow interrupts
+  TCCR1A = 0x00;    //timer control registers set for
+  TCCR1B = 0x00;    //normal operation, timer disabled  
   lcd.begin(16, 2);
-  lcd.print("======ITNY======");
+  lcd.print("      ITNY      ");
+  lcd.setCursor(0, 1);
+  lcd.print("   YOGYAKARTA   ");
   lcd.clear();
 }
-
-void loop(){
-  if(wait){
-    delayMicroseconds(wait);
-    wait=0;
-    digitalWrite(gatePin,HIGH);
-    delay(2);
-    digitalWrite(gatePin,LOW);
-  }
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    float RPM = counterEncoder * 2.5;//((counterEncoder/48)*120);
-    counterEncoder=0;
-    lcd.setCursor(0, 1);
-    lcd.print("RPM = ");
-    lcd.print(RPM);
-    lcd.setCursor(0, 0);
-    lcd.print("SUDUT = ");
-    PotValue = analogRead(potensio);
-    //Timer1.setPeriod(PotValue*10);
-    sudut = map(PotValue, 0, 1023, 0, 180);
-    lcd.print(sudut);
-  }
+void loop(){  
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      float RPM = counterEncoder * 2.5;//(counterEncoder/jumlahLubang)*(60000/interval);
+      counterEncoder=0;
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("RPM = ");
+      lcd.print(RPM);
+      PotValue = analogRead(potensio);
+      OCR1A =1 +( PotValue/1.6 );
+      sudut = map(PotValue, 0, 1000, 0, 180);
+      lcd.setCursor(0, 0);
+      lcd.print("SUDUT = ");
+      lcd.print(sudut);
+    }
 }
